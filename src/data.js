8e31ab76a4dd36a2572d35a2f9673ed48fc175f0
Connor McCue <connor.mccue@comcast.net>
2022-09-29 01:17:06 -0700

    removed useless object property

--- src/data.js
+++ src/data.js
@@ -1,63 +1,50 @@
 export const zodiacs = [
   {
-    name: 'aquarius',
-    sign: 'water',
+    name: 'Aquarius',
     date: '20 Jan - 19 Feb',
   },
   {
-    name: 'pisces',
-    sign: 'fish',
+    name: 'Pisces',
     date: '19 Feb - 21 Mar',
   },
   {
-    name: 'aries',
-    sign: 'ram',
+    name: 'Aries',
     date: '21 Mar - 20 Apr',
   },
   {
     name: 'Taurus',
-    sign: 'bull',
     date: '20 Apr - 21 May',
   },
   {
     name: 'Gemini',
-    sign: 'twins',
     date: '21 May - 21 Jun',
   },  
   {
     name: 'Cancer',
-    sign: 'crab',
     date: '21 Jun - 23 Jul',
   },
   {
     name: 'Leo',
-    sign: 'lion',
     date: '23 Jul - 23 Aug',
   },
   {
     name: 'Virgo',
-    sign: 'maiden',
     date: '23 Aug - 23 Sep',
   },
   {
     name: 'Libra',
-    sign: 'scales',
     date: '23 Sep - 23 Oct',
   },
   {
     name: 'Scorpio',
-    sign: 'scorpion',
     date: '23 Oct - 23 Nov',
   },
   {
     name: 'Sagittarius',
-    sign: 'archer',
     date: '23 Nov - 22 Dec',
   },  
   {
     name: 'Capricorn',
-    sign: 'goat',
     date: '22 Dec - 20 Jan',
   },
-
 ];
